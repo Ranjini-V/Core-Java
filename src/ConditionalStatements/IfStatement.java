941cc93bee7238bd8b999bbf7d7d46d2a9d1e0b7
Ranjini-V <ranjini.v.iyer35@gmail.com>
2025-12-26 02:09:49 +0530

    Initial commit – Core Java project

---
+++ src/ConditionalStatements/IfStatement.java
@@ -0,0 +1,18 @@
+package ConditionalStatements;
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+		
+		int age = 8;
+		
+		
+		if(age >=18) {
+			System.out.print("Eligible to vote");
+		} else {
+			System.out.print("Not eligible to vote");
+		}
+
+	}
+
+}
