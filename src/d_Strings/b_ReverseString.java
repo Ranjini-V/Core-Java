8e78a0f6e129b8d44e6d76b7ba9ceb1af6d06fc5
Ranjini-V <ranjini.v.iyer35@gmail.com>
2025-12-31 23:43:04 +0530

    String Reverse

--- src/d_Strings/b_ReverseString.java
+++ src/d_Strings/b_ReverseString.java
@@ -1,19 +1,31 @@
 package d_Strings;
 
+import java.util.Arrays;
+
 public class b_ReverseString {
 
 	public static void main(String[] args) {
 
-		// Approach 1
+//		 USING FOR LOOP
 
 		String s = "Lorem ipsum dolor sit amet, consectetuer";
-		String reversed = "";
+		String for_reversed = "";
 
 		for (int i = (s.length()) - 1; i >= 0; i--) {
-			reversed = reversed + s.charAt(i);
-
+			for_reversed = for_reversed + s.charAt(i);
 		}
-		System.out.println("The Reveresed String is: " + reversed);
+		System.out.println("The Reveresed String using for loop is: " + for_reversed);
+		System.out.println();
+
+//		 USING STRING METHODS - CONVERSION OF STRING INTO ARRAY
+		String method_reversed = "";
+		char a[] = s.toCharArray();
+
+		for (int i = (a.length) - 1; i >= 0; i--) {
+			method_reversed = method_reversed + a[i];
+		}
+		System.out.println("The Reveresed String using String method is: " + method_reversed);
+		System.out.println();
 
 	}
 
