405760fe135b52fd6a6a236a421c3b7e60fc8493
Ranjini-V <ranjini.v.iyer35@gmail.com>
2026-01-03 01:04:49 +0530

    String Buffer examples

---
+++ src/d_Strings/f_StringBuffer_Examples.java
@@ -0,0 +1,40 @@
+package d_Strings;
+
+public class f_StringBuffer_Examples {
+
+	public static void main(String[] args) {
+
+//		No new object is created for every modification
+		StringBuffer sb = new StringBuffer("Lorem Ipsum");
+
+//		STRING APPEND
+		System.out.println("Before Append: " + sb);
+		sb.append(" ABCD");
+		System.out.println("After Append: " + sb);
+
+//		STRING INSERT
+		sb.insert(0, " Inserted ");
+		System.out.println("After Insert: " + sb);
+
+//		STRING REPEAT
+		sb.repeat("Ipsum", 10);
+		System.out.println("After repeat: " + sb);
+
+//		STRING REPLACE
+		sb.replace(0, 78, "REPLACED");
+		System.out.println("After Replace: " + sb);
+
+//		CHARACTER DELETE
+		sb.deleteCharAt(7);
+		System.out.println("After delete: " + sb);
+
+//		STRING REPLACE
+		sb.reverse();
+		System.out.println("After reverse: " + sb);
+
+//		CHARACTER REPLACE
+		sb.setCharAt(0, 'F');
+		System.out.println("After character replace: " + sb);
+
+	}
+}
