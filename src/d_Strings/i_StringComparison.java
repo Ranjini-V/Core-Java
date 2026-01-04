1bea94252ba05e5ce5d488c0510c94414b5afde1
Ranjini-V <ranjini.v.iyer35@gmail.com>
2026-01-04 23:45:29 +0530

    String Comparison

--- src/d_Strings/i_StringComparison.java
+++ src/d_Strings/i_StringComparison.java
@@ -16,7 +16,15 @@ public class i_StringComparison {
 		System.out.println("String literal comparison: Using equals method: " + (var_s1.equals(var_s2)));
 		System.out.println();
 
+//		.equals()
+//		.equals() checks content/value of objects
+//		.equals() for object comparison (like String)
 
+		String obj_s1 = new String("Lorem Ipsum Dolor");
+		String obj_s2 = new String("Lorem Ipsum DOlor");
+
+		System.out.println("String objects comparison: Using ==: " + (obj_s1 == obj_s2));
+		System.out.println("String objects comparison: Using equals method: " + (obj_s1.equals(obj_s1)));
 	}
 
 }
