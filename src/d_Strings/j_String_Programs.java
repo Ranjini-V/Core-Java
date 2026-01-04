da205f5c81a76d45f0894e9b4d3d198714d385e4
Ranjini-V <ranjini.v.iyer35@gmail.com>
2026-01-05 01:23:02 +0530

    String Pratice

--- src/d_Strings/j_String_Programs.java
+++ src/d_Strings/j_String_Programs.java
@@ -1,24 +1,39 @@
 package d_Strings;
 
+import java.util.Arrays;
+
 public class j_String_Programs {
 
 	public static void main(String[] args) {
 
-//		CHECK WHETHER A STRING IS PALINDROME OR NOT
-		String pal_s = "LIRIL";
-		String rev = "";
+////		CHECK WHETHER A STRING IS PALINDROME OR NOT
+//		String pal_s = "LIRIL";
+//		String rev = "";
+//
+//		for (int i = (pal_s.length()) - 1; i >= 0; i--) {
+//			rev = rev + pal_s.charAt(i);
+//		}
+//		System.out.println("Reversed String is: " + rev);
+//
+//		if (pal_s.equals(rev)) {
+//			System.out.println("String is Palindrome");
+//		} else {
+//			System.out.println("String is not a Palindrome");
+//		}
+//		System.out.println();
 
-		for (int i = (pal_s.length()) - 1; i >= 0; i--) {
-			rev = rev + pal_s.charAt(i);
-		}
-		System.out.println("Reversed String is: " + rev);
+//		REMOVE JUNK/ SPECIAL CHARACTERS FROM THE STRING
 
-		if (pal_s.equals(rev)) {
-			System.out.println("String is Palindrome");
-		} else {
-			System.out.println("String is not a Palindrome");
+		String s = "@#LOREM IPSUM";
+		String spcl_string = "~`!@#$%^&*()_+=-{}|[]\":;'><,.?/";
+
+		char[] spclArray = spcl_string.toCharArray();
+
+		for (char spcl : spclArray) {
+			s = s.replace(String.valueOf(spcl), "");
 		}
-		System.out.println();
+
+		System.out.println("Clean String: " + s);
 
 	}
 
