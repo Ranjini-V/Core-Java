c56cce1003632e4b194d0e78ab2c0b01530d0ee8
Ranjini-V <ranjini.v.iyer35@gmail.com>
2026-01-05 23:57:07 +0530

    String Pratice

--- src/d_Strings/j_String_Programs.java
+++ src/d_Strings/j_String_Programs.java
@@ -6,21 +6,21 @@ public class j_String_Programs {
 
 	public static void main(String[] args) {
 
-////		CHECK WHETHER A STRING IS PALINDROME OR NOT
-//		String pal_s = "LIRIL";
-//		String rev = "";
-//
-//		for (int i = (pal_s.length()) - 1; i >= 0; i--) {
-//			rev = rev + pal_s.charAt(i);
-//		}
-//		System.out.println("Reversed String is: " + rev);
-//
-//		if (pal_s.equals(rev)) {
-//			System.out.println("String is Palindrome");
-//		} else {
-//			System.out.println("String is not a Palindrome");
-//		}
-//		System.out.println();
+//		CHECK WHETHER A STRING IS PALINDROME OR NOT
+		String pal_s = "LIRIL";
+		String rev = "";
+
+		for (int i = (pal_s.length()) - 1; i >= 0; i--) {
+			rev = rev + pal_s.charAt(i);
+		}
+		System.out.println("Reversed String is: " + rev);
+
+		if (pal_s.equals(rev)) {
+			System.out.println("String is Palindrome");
+		} else {
+			System.out.println("String is not a Palindrome");
+		}
+		System.out.println();
 
 //		REMOVE JUNK/ SPECIAL CHARACTERS FROM THE STRING
 
@@ -34,7 +34,14 @@ public class j_String_Programs {
 		}
 
 		System.out.println("Clean String: " + s);
+		System.out.println();
+
+//		REMOVE WHITE SPACES IN A STRING
+
+		String w = "LO REM  IPSU M";
+		String clean = w.replace(" ", "");
+		System.out.println("REMOVED WHITESPACE: " + clean);
+
 
 	}
-
 }
