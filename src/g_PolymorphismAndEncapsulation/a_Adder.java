7c5c7195f5d1d6fd27367f27f6d7d385e74d8c3c
Ranjini-V <ranjini.v.iyer35@gmail.com>
2026-01-11 22:41:53 +0530

    Polymorphism And Encapsulation

--- src/g_PolymorphismAndEncapsulation/a_Adder.java
+++ src/g_PolymorphismAndEncapsulation/a_Adder.java
@@ -6,6 +6,7 @@ public class a_Adder {
 
 	void sum() {
 		System.out.println("Default parameters");
+		System.out.println("Parameters are: " + a + " and " + b);
 		System.out.println(a + b);
 		System.out.println();
 	}
@@ -13,29 +14,30 @@ public class a_Adder {
 //	CHANGE IN NUMBER OF PARAMETERS
 	void sum(int x, int y) {
 		System.out.println("int x, int y");
+		System.out.println("Parameters are: " + x + " and " + y);
 		System.out.println(x + y);
 		System.out.println();
 	}
 
-	
 	void sum(int x, int y, int z) {
 		System.out.println("int x, int y, int z");
+		System.out.println("Parameters are: " + x + " , " + y + " and " + z);
 		System.out.println(x + y + z);
 		System.out.println();
 	}
 
-	
 //	CHANGE IN DATA TYPE OF PARAMETERS
 	void sum(long x, int y) {
 		System.out.println("long x, int y");
+		System.out.println("Parameters are: " + x + " and " + y);
 		System.out.println(x + y);
 		System.out.println();
 	}
 
-	
 //	CHANGE IN ORDER OF PARAMETERS
 	void sum(int y, long x) {
 		System.out.println("int y, long x");
+		System.out.println("Parameters are: " + x + " and " + y);
 		System.out.println(x + y);
 		System.out.println();
 	}
