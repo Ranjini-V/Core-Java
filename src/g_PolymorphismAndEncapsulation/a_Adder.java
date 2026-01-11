320ea32fae0936de2aa514b1439431268efa9ae1
Ranjini-V <ranjini.v.iyer35@gmail.com>
2026-01-11 22:37:42 +0530

    Polymorphism And Encapsulation

--- src/g_PolymorphismAndEncapsulation/a_Adder.java
+++ src/g_PolymorphismAndEncapsulation/a_Adder.java
@@ -5,29 +5,39 @@ public class a_Adder {
 	int a = 10, b = 30;
 
 	void sum() {
+		System.out.println("Default parameters");
 		System.out.println(a + b);
+		System.out.println();
 	}
 
 //	CHANGE IN NUMBER OF PARAMETERS
 	void sum(int x, int y) {
+		System.out.println("int x, int y");
 		System.out.println(x + y);
+		System.out.println();
 	}
 
 	
 	void sum(int x, int y, int z) {
+		System.out.println("int x, int y, int z");
 		System.out.println(x + y + z);
+		System.out.println();
 	}
 
 	
 //	CHANGE IN DATA TYPE OF PARAMETERS
 	void sum(long x, int y) {
+		System.out.println("long x, int y");
 		System.out.println(x + y);
+		System.out.println();
 	}
 
 	
 //	CHANGE IN ORDER OF PARAMETERS
 	void sum(int y, long x) {
+		System.out.println("int y, long x");
 		System.out.println(x + y);
+		System.out.println();
 	}
 
 }
