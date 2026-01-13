1667c42f3caf7e3d2343fc83c5a33cc2e7354225
Ranjini-V <ranjini.v.iyer35@gmail.com>
2026-01-13 23:49:57 +0530

    Polymorphism And Encapsulation

--- src/g_PolymorphismAndEncapsulation/d_ConstructorOverloading_BoxMain.java
+++ src/g_PolymorphismAndEncapsulation/d_ConstructorOverloading_BoxMain.java
@@ -7,9 +7,13 @@ public class d_ConstructorOverloading_BoxMain {
 		c_ConstructorOverloading_Box box = new c_ConstructorOverloading_Box();
 		System.out.println("Default Constructor Overloaded: ");
 		System.out.println(box.volume());
+		System.out.println();
 		
 		
 		
+		c_ConstructorOverloading_Box box2 = new c_ConstructorOverloading_Box(10,20,30);
+		System.out.println("Prameterized Constructor Overloaded: ");
+		System.out.println(box2.volume());
 
 	}
 
